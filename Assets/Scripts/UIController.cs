30c3b8aae67c70e1d07dd67ea4d45b80f2d16d1a
DevAngeloGoncalo <angelo.goncalo87@gmail.com>
2022-02-05 20:28:09 -0300

    Revert "FadeIn Death Text"
    This reverts commit 3d277b800a1a75c8bdb32ff8d9f5ad48c57e6335.

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public static UIController instance;
-    public static int scorePoints = 0; //Static para ser acessada na classe Enemy
 
+    public static int scorePoints = 0; //Static para ser acessada na classe Enemy
     public Text textScore;
+
     public GameObject deathScreen;
-    public Image fadeScreen;
 
     private void Awake() //necessary to active objects in other classes
     {
@@ -20,12 +20,12 @@ public class UIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        textScore.text = "Score: " + scorePoints.ToString("00000");
+        textScore.text = "Score: " + scorePoints.ToString("00000");   
     }
 }
